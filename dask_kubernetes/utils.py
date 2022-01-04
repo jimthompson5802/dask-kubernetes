24d90a57fc6f5b1ef2e18c45d588ef5ee86309a4
Jim Thompson <jimthompson5802@gmail.com>
2022-01-04 12:49:21 -0500

    allow both native k8s and istio compliant port names

--- dask_kubernetes/utils.py
+++ dask_kubernetes/utils.py
@@ -44,12 +44,12 @@ async def get_external_address_for_scheduler_service(
     core_api, service, port_forward_cluster_ip=None
 ):
     """Take a service object and return the scheduler address."""
-    # TODO: port.name changed from comm to tcp-comm for kubeflow
-    #       need to assess impact for vanilla k8s cluster setup
+    # add 'tcp-comm' as valid port name when istio is in use
     [port] = [
         port.port
         for port in service.spec.ports
-        if port.name == service.metadata.name or port.name == "tcp-comm"
+        if port.name == service.metadata.name or port.name == "comm"
+        or port.name == "tcp-comm"
     ]
     if service.spec.type == "LoadBalancer":
         lb = service.status.load_balancer.ingress[0]
